143d85a5477c5289f2a366d69d8983c506202421
erik-K <e.keckes8@gmail.com>
2022-06-27 12:38:11 +0200

    cthulu blink

---
+++ Assets/Scripts/Map/CthuluAnimations.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CthuluAnimations : MonoBehaviour
+{
+    public Animator anim;
+    [Range(3,10)]public float min;
+    [Range(1,2)]public float max;
+    void Start()
+    {
+        StartCoroutine(BlinkTimer());
+    }
+    IEnumerator BlinkTimer()
+    {
+        while (true)
+        {
+            float wait = Random.Range(min, max);
+            print(wait);
+            yield return new WaitForSeconds(wait);
+            print(anim.GetCurrentAnimatorStateInfo(0).IsName("Watching"));
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Watching"))
+            {
+                Blink();
+            }
+        }
+    }
+    void Blink()
+    {
+        anim.Play("Blink");
+    }
+}
